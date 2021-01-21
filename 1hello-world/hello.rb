571c80d2f70730aba6abe631d3bcd98230d29b99
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-01-21 22:07:24 +0700

    hello world, variables and variables

---
+++ 1hello-world/hello.rb
@@ -0,0 +1,10 @@
+puts 'hello world'
+
+firstName = 'riky'
+lastName = 'hidayat'
+
+if firstName == 'riky'
+  puts 'yes you are the next CTO'
+else
+  puts 'or you are the next CEO? who knows'
+end
---
+++ 1hello-world/variables.rb
@@ -0,0 +1,19 @@
+=begin
+Untuk membuat variable pada Ruby, kita tidak memerlukan syntax apapun,
+kita hanya perlu membuat nama variable yang kita inginkan
+=end
+
+myVariable = 'ini adalah variable yang saya buat'
+
+puts "saya akan memanggil #{myVariable}"
+#Output: saya akan memanggil ini adalah variable yang saya buat
+
+=begin
+cara saya diatas #{myVariable} yaitu sebuah syntax untuk memanggil variable yang telah kita buat
+=end
+
+firstName = "memet"
+age = 23
+
+puts "my name is #{firstName} and I'm #{age} years old."
+#output: my name is memet and I'm 23 years old.
